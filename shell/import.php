c81f48440b3dd17bf7cb148495cc34daf466a09d
root <root@training3.training3>
2015-11-23 23:41:13 -0800

    update

--- shell/import.php
+++ shell/import.php
@@ -21,11 +21,6 @@ exit;
  */
 function perform()
 {
-    if ( phpversion() < '5.5' ) {
-        pr("PHP Version need >= 5.5");
-        exit;
-    }
-
     if (!getParam('exec')) {
         pr('---- debug mode ---- (你必須要輸入參數 exec 才會真正執行)');
     }
